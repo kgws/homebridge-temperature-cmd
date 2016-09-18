6513e09a9722960ef12454c9f8460f877c7ae68f
kgws <dev.kgws@gmail.com>
2016-09-18 22:25:40 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# homebridge-temperature-cmda
+# homebridge-temperature-cmd
+homebridge-plugin for Your PC Command with Apple-Homekit.(by node.js child_process.exec())
+
+# Installation
+
+1. Install homebridge using: sudo npm install -g homebridge
+2. Install this plugin using: sudo npm install -g homebridge-temperature-cmd
+3. Update your configuration file. See sample-config.json in this repository for a sample.
+
+# Configuration
+
+Configuration sample:
+
+```
+"accessories": [
+    {
+        "accessory": "TemperatureCMD",
+        "name": "Living Room Temperature",
+        "command": "sudo /usr/local/bin/temper | cut -d ',' -f 2"
+    }
+]
+
---
+++ index.js
@@ -0,0 +1,70 @@
+var Service, Characteristic;
+var exec = require("child_process").exec;
+
+var temperatureService;
+var command;
+var temperature = 0;
+
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("homebridge-temperature-cmd", "TemperatureCMD", TemperatureCMD);
+}
+
+function TemperatureCMD(log, config) {
+    this.log = log;
+
+    // url info
+    this.name = config["name"];
+    this.manufacturer = config["manufacturer"] || "Luca Manufacturer";
+    this.model = config["model"] || "Luca Model";
+    this.serial = config["serial"] || "Luca Serial";
+    this.command = config["command"];
+}
+
+TemperatureCMD.prototype = {
+    cmdRequest: function(cmd, callback) {
+        exec(cmd,function(error, stdout, stderr) {
+            callback(error, stdout, stderr)
+        })
+    },
+
+    getState: function (callback) {
+        var cmd = this.command;
+        this.cmdRequest(cmd, function(error, stdout, stderr) {
+            if (error) {
+                this.log('command function failed: %s', stderr);
+                callback(error);
+            } else {
+                this.log('command function succeeded!');
+                var res =  Math.round(stdout * 10) /10;
+                this.log(res);
+                callback(null, res);
+            }
+        }.bind(this));
+    },
+
+    identify: function(callback) {
+        this.log("Identify requested!");
+        callback(); // success
+    },
+
+    getServices: function() {
+        var services = [],
+            informationService = new Service.AccessoryInformation();
+
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+            .setCharacteristic(Characteristic.Model, this.model)
+            .setCharacteristic(Characteristic.SerialNumber, this.serial);
+        services.push(informationService);
+
+        temperatureService = new Service.TemperatureSensor(this.name);
+        temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .on('get', this.getState.bind(this));
+        services.push(temperatureService);
+
+        return services;
+    }
+};
